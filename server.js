4c3ae12c8dc607abad13d79036d3c5d7de1ccff0
A1dos-Creations <unrelatablexfilms@gmail.com>
2025-03-05 19:21:37 -0800

    Refactor password change verification email layout for improved readability and user experience

--- server.js
+++ server.js
@@ -65,34 +65,39 @@ app.post('/send-verification-code', async (req, res) => {
             from: 'admin@a1dos-creations.com',
             subject: `${user.name} Password Change Verification Code`,
             html: `
-                <tr height="32" style="height:32px"><td></td></tr>
+                <tr height="32" style="height:32px">
+                <td></td>
+                </tr>
                 <tr align="center">
-                <table border="0" cellspacing="0" cellpadding="0" style="padding-bottom:20px;max-width:516px;min-width:220px">
-                <tbody>
-                <tr>
-                <td width="8" style="width:8px"></td>
-                <td>
-
-                <br>
-                <br>
-                <div style="border-style:solid;border-width:thin;border-color:#dadce0;border-radius:8px;padding:40px 20px" align="center">
-                <div style="font-family:Roboto,RobotoDraft,Helvetica,Arial,sans-serif;border-bottom:thin solid #dadce0;color:rgba(0,0,0,0.87);line-height:32px;padding-bottom:24px;text-align:center;word-break:break-word">
-                <div style="font-size:24px"><strong>Password Change Verification Request</strong></div>
-                <div style="font-size:24px">For user: <strong>${user.name}</strong></div>
-                <div style="font-size:24px">Your verification code is: <strong>${verificationCode}</strong></div>
-                <table align="center" style="margin-top:8px">
-                <tbody><tr style="line-height:normal">
-                <td align="right" style="padding-right:8px">
-                </td>
+                  <div>
+                    <div>
+                    </div>
+                  </div>
+                  <table border="0" cellspacing="0" cellpadding="0" style="padding-bottom:20px;max-width:516px;min-width:220px">
+                  <tbody>
+                  <tr>
+                  <td width="8" style="width:8px"></td>
+                  <td>
+                    <div style="border-style:solid;border-width:thin;border-color:#dadce0;border-radius:8px;padding:40px 20px" align="center">
+                      <div style="font-family:Roboto,RobotoDraft,Helvetica,Arial,sans-serif;border-bottom:thin solid #dadce0;color:rgba(0,0,0,0.87);line-height:32px;padding-bottom:24px;text-align:center;word-break:break-word">
+                      <div style="font-size:24px"><strong>Password Change Verification Request</strong></div>
+                      <div style="font-size:24px">For user: <strong>${user.name}</strong></div>
+                      <div style="font-size:24px">Your verification code is: <strong>${verificationCode}</strong></div>
+                      <table align="center" style="margin-top:8px">
+                      <tbody><tr style="line-height:normal">
+                      <td align="right" style="padding-right:8px">
+                      </td>
+                      </tr>
+                      </tbody>
+                      </table>
+                    </div>
+                  <div style="font-family:Roboto-Regular,Helvetica,Arial,sans-serif;font-size:14px;color:rgba(0,0,0,0.87);line-height:20px;padding-top:20px;text-align:left"><br>If this was not you, ignore this email. We will never ask for your password or verification code.<div style="padding-top:32px;text-align:center"><a href="https://a1dos-creations.com/account/account" style="font-family:'Google Sans',Roboto,RobotoDraft,Helvetica,Arial,sans-serif;line-height:16px;color:#ffffff;font-weight:400;text-decoration:none;font-size:14px;display:inline-block;padding:10px 24px;background-color:#4184f3;border-radius:5px;min-width:90px" target="_blank">Check activity</a>
+                  </div>
+                  </div>
                 </tr>
-                </tbody>
-                </table>
-                </div>
-                <div style="font-family:Roboto-Regular,Helvetica,Arial,sans-serif;font-size:14px;color:rgba(0,0,0,0.87);line-height:20px;padding-top:20px;text-align:left"><br>If this was not you, ignore this email. We will never ask for your password or verification code.<div style="padding-top:32px;text-align:center"><a href="https://a1dos-creations.com/account/account" style="font-family:'Google Sans',Roboto,RobotoDraft,Helvetica,Arial,sans-serif;line-height:16px;color:#ffffff;font-weight:400;text-decoration:none;font-size:14px;display:inline-block;padding:10px 24px;background-color:#4184f3;border-radius:5px;min-width:90px" target="_blank">Check activity</a>
-                </div>
-                </div>
+                <tr height="32" style="height:32px">
+                  <td></td>
                 </tr>
-                <tr height="32" style="height:32px"><td></td></tr>
                 `,
                 trackingSettings: {
                   clickTracking: { enable: false, enableText: false }
