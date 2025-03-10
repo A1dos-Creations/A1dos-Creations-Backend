8f707b001b32c081ef85212492413a3964dbbfb9
A1dos-Creations <unrelatablexfilms@gmail.com>
2025-03-10 16:51:05 -0700

    Add logging for Twilio SID and Auth Token in server.js

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ import sgMail from '@sendgrid/mail';
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 import twilio from 'twilio';
 const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+console.log("TWILIO_SID:", process.env.TWILIO_SID);
+console.log("TWILIO_AUTH_TOKEN:", process.env.TWILIO_AUTH_TOKEN);
 
 const verificationCodes = new Map(); 
 
