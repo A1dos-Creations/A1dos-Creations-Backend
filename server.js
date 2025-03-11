edf7057214a03d6665212fe83537afc06c71a74b
A1dos-Creations <unrelatablexfilms@gmail.com>
2025-03-10 21:18:16 -0700

    Remove Firebase initialization and configuration from server.js

--- server.js
+++ server.js
@@ -12,22 +12,6 @@ import Stripe from 'stripe';
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 import sgMail from '@sendgrid/mail';
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDN1E5nYSj0aJQ66EEFf8NGH3lkDy64Cf8",
-  authDomain: "a1dos-creations-25.firebaseapp.com",
-  projectId: "a1dos-creations-25",
-  storageBucket: "a1dos-creations-25.firebasestorage.app",
-  messagingSenderId: "874801347017",
-  appId: "1:874801347017:web:8c63552a4bde3c519a9018",
-  measurementId: "G-H3HVX1034N"
-};
-
-// Initialize Firebase
-const appF = initializeApp(firebaseConfig);
-const analytics = getAnalytics(appF);
 
 const allowedOrigins = [
   'https://a1dos-creations.com',
